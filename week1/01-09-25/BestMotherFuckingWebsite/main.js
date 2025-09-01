f54e03e5a377f3a278b0063c23c5b6935383aeca
Sophie Mourre <sophie@mourre.net>
2025-09-01 21:35:17 +0200

    Playing with JS buttons

--- week1/01-09-25/BestMotherFuckingWebsite/main.js
+++ week1/01-09-25/BestMotherFuckingWebsite/main.js
@@ -1,7 +1,15 @@
 const buttonToggle = document.querySelector('.toggle');
 buttonToggle.addEventListener('click', toggleClass);
 
+const buttonIncreaseContrast = document.querySelector('.increase-contrast');
+buttonIncreaseContrast.addEventListener('click', increaseContrast);
+
 function toggleClass(){
   const element = document.querySelector('body');
-  element.classList.toggle('new-class');
+  element.classList.toggle('toggle-light');
+}
+
+function increaseContrast(){
+    const body = document.querySelector('body');
+    body.classList.toggle('high-contrast')
 }
